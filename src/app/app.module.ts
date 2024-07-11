f99f0a0acdefdfa990b195f395d02affb9300f6e
fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2024-07-10 23:24:38 -0300

    Component login - Routing login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './services/interceptor-jwt.service';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
